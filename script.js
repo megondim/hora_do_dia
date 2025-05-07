bdc36908856c3e46c635d41039529fffafbee397
Maria Eduarda Gondim <gondimme@gmail.com>
2025-05-07 01:20:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function carregar(){
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 19
+    msg.innerHTML = `Agora são ${hora} horas...`
+    if(hora >= 5 && hora < 12){
+        img.src = 'manha.jpg'
+        document.body.style.background = '#e7c144'
+    } else if (hora >= 12 && hora < 18){
+        img.src = 'tarde.jpg'
+        document.body.style.background = '#e77244'
+    } else{
+        img.src = 'noite.jpg'
+        document.body.style.background = '#4532f1'
+    }
+}
+
