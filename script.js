c03698a357bc2cfd8b5cb0d8be8cb1c92b4cfb44
Maria Eduarda Gondim <gondimme@gmail.com>
2025-05-07 01:29:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ function carregar(){
     var msg = document.getElementById('msg')
     var img = document.getElementById('imagem')
     var data = new Date()
-    //var hora = data.getHours()
-    var hora = 19
+    var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas...`
     if(hora >= 5 && hora < 12){
         img.src = 'manha.jpg'
